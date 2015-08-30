d4ed5b764193b92f352a46b65d3382907827e9f5
Vigith Maurice <vigith@gmail.com>
2015-08-30 13:53:39 -0700

    faster echo server

--- echo_server/echo_server.ino
+++ echo_server/echo_server.ino
@@ -1,12 +1,16 @@
 #define DEBUG 1
 #define PORT 8888
 
-#define SSID     "xxxxx"
-#define SSPASSWD "XXXXXXX"
+#define SSID     "--"
+#define SSPASSWD "--"
 
 char rn[] = "\r\n";
-char ok[] = "OK";
-char ready[] = "ready";
+char ok[] = "OK\r\n";
+char ready[] = "ready\r\n";
+
+#define BUFFER_SIZE 512
+/* data when processing loop */
+char buffer[BUFFER_SIZE];
 
 /* sends a command and test whether we got the ack string */
 boolean checkSendDataAck(String command, char *ack, const int timeout) {
@@ -15,14 +19,22 @@ boolean checkSendDataAck(String command, char *ack, const int timeout) {
   if (DEBUG)
     Serial.print(command);
   
-  /* set timeout */
-  Serial1.setTimeout(timeout);
-  
   /* write to module */
   Serial1.print(command); // send the read character to the Serial1
 
-  if(Serial1.find(ack)) {          
+    /* set timeout */
+  Serial1.setTimeout(timeout);
+
+  unsigned long time = millis();
+  while (time + timeout > millis()) {
+    if(Serial1.available()) {
+      if(Serial1.find(ack)) {          
         flag = true;
+        break;
+      }
+      if (flag)
+        break;
+    }
   }
 
   return flag;
@@ -59,7 +71,7 @@ void checkSendDataDebug(String command, const int timeout) {
 
   unsigned long time = millis();
   while (time + timeout > millis()) {
-    while(Serial1.available()) {
+    if(Serial1.available()) {
       char data = (char)Serial1.read();
       Serial.write(data);
     }
@@ -68,14 +80,23 @@ void checkSendDataDebug(String command, const int timeout) {
   return;
 }
 
-/* sends a command and test whether we got the ack string */
+/* clear the buffer in esp8266 */
 boolean clearBuffer(const int timeout) {
   boolean flag = false;
   /* set timeout */
   Serial1.setTimeout(timeout);
 
-  if(Serial1.find(rn)) {
-    flag = true;
+
+  unsigned long time = millis();
+  while (time + timeout > millis()) {
+    if(Serial1.available()) {
+      if(Serial1.find(rn)) {          
+        flag = true;
+        break;
+      }
+      if (flag)
+        break;
+    }
   }
 
   return flag;
@@ -93,6 +114,7 @@ boolean startServer(void) {
     if (DEBUG)
       Serial.println("retry");
 
+
   /* configure it as a client */
   command = "AT+CWMODE=1\r\n";
   while((status = checkSendDataAck(command, ok, 2000)) == false && retry-- > 0)
@@ -102,7 +124,7 @@ boolean startServer(void) {
   command = "AT+CWJAP?\r\n";
   char ack[] = SSID;
   status = checkSendDataAck(command, ack, 2000);
-  status = clearBuffer(1000);
+  clearBuffer(1000);
 
   /* if not connected, try to connect */
   if (not status) {
@@ -113,7 +135,8 @@ boolean startServer(void) {
       command += SSPASSWD;
       command += "\"\r\n";
       status = false; retry = 4;
-      while((status = checkSendDataAck(command, ok, 2000)) == false && retry-- > 0)
+      checkSendDataDebug(command, 5000);
+      while((status = checkSendDataAck(command, ok, 5000)) == false && retry-- > 0)
         if (DEBUG)
           Serial.println("retry");
   }
@@ -123,8 +146,9 @@ boolean startServer(void) {
   status = getIpAddress(1000, &ip);
 
 
-  /* allow it for multiconnection */
+  /* allow it for single IP connection */
   command = "AT+CIPMUX=1\r\n";
+  status = false; retry = 4;
   while((status = checkSendDataAck(command, ok, 2000)) == false && retry-- > 0)
     if (DEBUG)
       Serial.println("retry");
@@ -133,6 +157,7 @@ boolean startServer(void) {
   command = "AT+CIPSERVER=1,";
   command += PORT;
   command += "\r\n";
+  status = false; retry = 4;
   while((status = checkSendDataAck(command, ok, 2000)) == false && retry-- > 0)
     if (DEBUG)
       Serial.println("retry");
@@ -151,17 +176,110 @@ void setup() {
 
   /* connect to wifi */
   status = startServer();
-  if (DEBUG) {
+  if (DEBUG)
     Serial.println(status);
+
+  if (status) {
+      Serial.println("Started Server");        
   } else {
     while(1);
   }
 
-  Serial.println("Started Server");
+}
+
+/* read the EOL, ie \r\n */
+bool readTillEol() {
+  /* static is used because we might leave out of this function before getting all the chars, in that case
+     we want to know the buffer postn and push more data from where we left off. */
+  static int i=0;
+  
+  /* data is available */
+  if(Serial1.available()) {    
+    /* push to buffer */
+    buffer[i++] = Serial1.read();
+    
+    /* overflow, reset to top */
+    if(i == BUFFER_SIZE)
+      i = 0;
+    
+    // 'i' should be atleast 2 (ie, > 1) to test for a successful \r\n
+    // 13 = cr and 10 = nl
+    if(i>1 && buffer[i-2]==13 && buffer[i-1]==10) {
+      /* set to \0 */
+      buffer[i]=0;
+      /* set i = 0, we have read \r\n, next invocation of readTillEol is for a fresh buffer */
+      i = 0;
+      
+      if (DEBUG)
+        Serial.print(buffer);
+      
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+void echo_back(int channel_id, int packet_len, char *buffer_ptr, const int timeout) {
+  boolean flag = false;
+  char send[] = "> ";
+  Serial.println(channel_id);
+  Serial.println(packet_len);
+  Serial.println(buffer_ptr);
+  
+  Serial1.print("AT+CIPSEND=");
+  Serial1.print(channel_id);
+  Serial1.print(",");
+  Serial1.print(packet_len);
+  Serial1.print("\r\n");
+
+  unsigned long time = millis();
+  while (time + timeout > millis()) {
+    if(Serial1.available()) {
+      Serial.print((char)Serial1.read());
+      if(Serial1.find(send)) {
+        flag = true;
+        break;
+      }
+      if (flag)
+        break;
+    }
+  }  
+
+  if (DEBUG)    
+    Serial.print(flag);
+
+  if (flag)
+    Serial1.print(buffer_ptr);
+
+  return;
 }
 
 void loop() {
-  if (Serial1.available()) {
-    Serial.write(Serial1.read());
+  /* store the channel_id and packet_len */
+  int channel_id, packet_len;
+  /* point to valid data in buffer */
+  char *buffer_ptr;
+
+  /* read the data */
+  if (readTillEol()) {
+    /* make sure the buffer has +IPD, ie data from client */
+    if(strncmp(buffer, "+IPD,", 5)==0) {
+      /* get the channel_id and packet_len
+         the buffer format is IPD+,channel_id,&packet_len:DATA*/
+      sscanf(buffer+5, "%d,%d", &channel_id, &packet_len);
+      if (packet_len > 0) {
+        /* jump +IPD, */
+        buffer_ptr = buffer+5;
+        /* start reading from ':' (skip the channel_id and packet_len) */
+        while(*buffer_ptr++!=':');
+        /* echo back */
+        echo_back(channel_id, packet_len, buffer_ptr, 10000);        
+    } else {
+        /* TODO: oops, we got some other data. do errors processing */
+        if (DEBUG)
+          Serial.println("ERR: IPD+");
+      }
+    }
   }
 }
